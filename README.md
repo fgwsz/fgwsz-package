5793dafc9251fd0a5044f4a900aee2b6fc54ba4c
fgwsz <3191000458@qq.com>
2025-12-28 02:37:43 +0800

    修正表述不合理的地方

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
     包的二进制的文件结构如下:
         [file item 1]...[file item N]
     每个文件的二进制信息[file item]的文件结构是[A|B|C|D]:
-        A部分是[path_size(8字节)]
-        B部分是[relative_path]
-        C部分是[content_size(8字节)]
+        A部分是[relative path bytes(8字节)]
+        B部分是[relative path]
+        C部分是[content bytes(8字节)]
         D部分是[content(binary)]
 ```
 使用`fgwsz-package`进行打包和解包  
--- source/fgwsz_package.cpp
+++ source/fgwsz_package.cpp
@@ -11,13 +11,13 @@
 #include<filesystem>//::std::filesystem
 
 //作者制定了归档一个或多个(文件/目录)的包标准:
-//  包的二进制的文件结构如下:
-//      [file item 1]...[file item N]
-//  每个文件的二进制信息[file item]的文件结构是[A|B|C|D]:
-//      A部分是[path_size(8字节)]
-//      B部分是[relative_path]
-//      C部分是[content_size(8字节)]
-//      D部分是[content(binary)]
+//    包的二进制的文件结构如下:
+//        [file item 1]...[file item N]
+//    每个文件的二进制信息[file item]的文件结构是[A|B|C|D]:
+//        A部分是[relative path bytes(8字节)]
+//        B部分是[relative path]
+//        C部分是[content bytes(8字节)]
+//        D部分是[content(binary)]
 
 //文件结构体
 struct File{
