8ddf3d7ce84150763e421ab998b06ed03c7caa54
fgwsz <3191000458@qq.com>
2026-01-10 00:03:32 +0800

    重构unpacker

--- source/fgwsz_unpacker.cpp
+++ source/fgwsz_unpacker.cpp
@@ -2,11 +2,14 @@
 
 #include<cstdint>   //::std::uint8_t ::std::uint64_t
 
-#include<string>    //::std::string
-#include<filesystem>//::std::filesystem
-#include<fstream>   //::std::ofstream ::std::ifstream
-#include<vector>    //::std::vector
-#include<memory>    //::std::unique_ptr
+#include<string>        //::std::string
+#include<filesystem>    //::std::filesystem
+#include<ios>           //::std::ios
+#include<fstream>       //::std::ofstream ::std::ifstream
+#include<vector>        //::std::vector
+#include<memory>        //::std::unique_ptr
+#include<type_traits>   //::std::remove_cvref_t
+#include<format>        //::std::format
 
 #include"fgwsz_endian.hpp"
 #include"fgwsz_except.h"
@@ -39,8 +42,18 @@ Unpacker::~Unpacker(void){
         this->package_.close();
     }
 }
-::std::uint8_t Unpacker::unpack_key(::std::uint64_t& package_count_bytes){
-    ::std::uint8_t key=0;
+void Unpacker::reset_package(void){
+    //重置包文件流位置到文件头
+    this->package_.seekg(0);
+    if(!this->package_.good()){
+        FGWSZ_THROW_WHAT(
+            "failed to jump package head: "+this->package_path_string_
+        );
+    }
+    //重置用于记录已读取包内容字节数的计数器
+    this->package_count_bytes_=0;
+}
+void Unpacker::unpack_key(::std::uint8_t& key){
     ::std::uint64_t read_bytes=::fgwsz::std_ifstream_read(
         this->package_
         ,(char*)&key
@@ -52,14 +65,12 @@ Unpacker::~Unpacker(void){
             "failed to read key: "+this->package_path_string_
         );
     }
-    package_count_bytes+=static_cast<::std::uint64_t>(read_bytes);
-    return key;
+    this->package_count_bytes_+=read_bytes;
 }
-::std::uint64_t Unpacker::unpack_relative_path_bytes(
-    ::std::uint64_t& package_count_bytes
-    ,::std::uint8_t key
+void Unpacker::unpack_relative_path_bytes(
+    ::std::uint8_t key
+    ,::std::uint64_t& relative_path_bytes
 ){
-    ::std::uint64_t relative_path_bytes=0;
     ::std::uint64_t read_bytes=::fgwsz::std_ifstream_read(
         this->package_
         ,(char*)&relative_path_bytes
@@ -72,27 +83,24 @@ Unpacker::~Unpacker(void){
             +this->package_path_string_
         );
     }
-    package_count_bytes+=static_cast<::std::uint64_t>(read_bytes);
+    this->package_count_bytes_+=read_bytes;
     //解码relative path bytes的文件密钥xor混淆
     //字节序辅助结构体
-    ::fgwsz::EndianHelper<decltype(relative_path_bytes)> net;
-    for(::std::uint8_t index=0;index<sizeof(relative_path_bytes);++index){
+    using number_type=::std::remove_cvref_t<decltype(relative_path_bytes)>;
+    ::fgwsz::EndianHelper<number_type> net;
+    for(::std::uint8_t index=0;index<sizeof(net);++index){
         net.byte_array[index]=static_cast<::std::uint8_t>(
             ((char*)&relative_path_bytes)[index]
         )^key;
     }
     //将网络序转换为主机序,得到relative path bytes
-    relative_path_bytes=
-        ::fgwsz::net_to_host<decltype(relative_path_bytes)>(net.data);
-    return relative_path_bytes;
+    relative_path_bytes=::fgwsz::net_to_host<number_type>(net.data);
 }
-::std::string Unpacker::unpack_relative_path_string(
-    ::std::uint64_t& package_count_bytes
-    ,::std::uint8_t key
+void Unpacker::unpack_relative_path_string(
+    ::std::uint8_t key
     ,::std::uint64_t relative_path_bytes
+    ,::std::string& relative_path_string
 ){
-    //读取relative path
-    ::std::string relative_path_string;
     relative_path_string.resize(relative_path_bytes);
     ::std::uint64_t read_bytes=::fgwsz::std_ifstream_read(
         this->package_
@@ -105,18 +113,16 @@ Unpacker::~Unpacker(void){
             "failed to read relative path: "+this->package_path_string_
         );
     }
-    package_count_bytes+=static_cast<::std::uint64_t>(read_bytes);
+    this->package_count_bytes_+=read_bytes;
     //解码relative path的文件密钥xor混淆
     for(char& ch:relative_path_string){
         ch=static_cast<char>(static_cast<::std::uint8_t>(ch)^key);
     }
-    return relative_path_string;
 }
-::std::uint64_t Unpacker::unpack_content_bytes(
-    ::std::uint64_t& package_count_bytes
-    ,::std::uint8_t key
+void Unpacker::unpack_content_bytes(
+    ::std::uint8_t key
+    ,::std::uint64_t& content_bytes
 ){
-    ::std::uint64_t content_bytes=0;
     ::std::uint64_t read_bytes=::fgwsz::std_ifstream_read(
         this->package_
         ,(char*)&content_bytes
@@ -128,48 +134,40 @@ Unpacker::~Unpacker(void){
             "failed to read content bytes: "+this->package_path_string_
         );
     }
-    package_count_bytes+=static_cast<::std::uint64_t>(read_bytes);
+    this->package_count_bytes_+=read_bytes;
     //解码content bytes的文件密钥xor混淆
     //字节序辅助结构体
-    ::fgwsz::EndianHelper<decltype(content_bytes)> net;
-    for(::std::uint64_t index=0;index<sizeof(content_bytes);++index){
+    using number_type=::std::remove_cvref_t<decltype(content_bytes)>;
+    ::fgwsz::EndianHelper<number_type> net;
+    for(::std::uint64_t index=0;index<sizeof(net);++index){
         net.byte_array[index]=
             static_cast<::std::uint8_t>(((char*)&content_bytes)[index])^key;
     }
     //将网络序转换为主机序,得到content bytes
-    content_bytes=::fgwsz::net_to_host<decltype(content_bytes)>(net.data);
-    return content_bytes;
+    content_bytes=::fgwsz::net_to_host<number_type>(net.data);
 }
-void Unpacker::unpack_header(
-    ::std::uint64_t& package_count_bytes
-    ,Header& header
-){
-    //读取key
-    header.key=this->unpack_key(package_count_bytes);
-    //读取relative path bytes
-    header.relative_path_bytes=
-        this->unpack_relative_path_bytes(package_count_bytes,key);
-    //读取relative path
-    header.relative_path_string=this->unpack_relative_path_string(
-        package_count_bytes,key,relative_path_bytes
+void Unpacker::unpack_header(Unpacker::Header& header){
+    this->unpack_key(header.key);
+    this->unpack_relative_path_bytes(header.key,header.relative_path_bytes);
+    this->unpack_relative_path_string(
+        header.key
+        ,header.relative_path_bytes
+        ,header.relative_path_string
     );
-    //读取content bytes
-    header.content_bytes=this->unpack_content_bytes(package_count_bytes,key);
+    this->unpack_content_bytes(header.key,header.content_bytes);
 }
 void Unpacker::unpack_content(
-    ::std::uint64_t& package_count_bytes
-    ,::std::uint8_t key
+    ::std::filesystem::path const& output_dir_path
+    ,Unpacker::Header const& header
     ,char* block
     ,::std::uint64_t block_bytes
-    ,::std::filesystem::path const& output_dir_path
-    ,::std::string const& relative_path_string
-    ,::std::uint64_t content_bytes
 ){
     //判断相对路径是否是安全路径
-    ::fgwsz::path_assert_is_safe_relative_path(relative_path_string);
+    ::fgwsz::path_assert_is_safe_relative_path(header.relative_path_string);
     //创建文件父目录
-    ::std::filesystem::path file_path=
-        ::std::filesystem::absolute(output_dir_path/relative_path_string);
+    ::std::filesystem::path file_path=::std::filesystem::absolute(
+        output_dir_path/header.relative_path_string
+    );
     ::fgwsz::try_create_directories(
         ::fgwsz::parent_path(file_path)
     );
@@ -182,20 +180,20 @@ void Unpacker::unpack_content(
     //分块读取content
     ::std::uint64_t file_count_bytes=0;
     ::std::uint64_t read_bytes=0;
-    while(file_count_bytes<content_bytes){
+    while(file_count_bytes<header.content_bytes){
         read_bytes=::fgwsz::std_ifstream_read(
             this->package_
             ,&(block[0])
-            ,((block_bytes>(content_bytes-file_count_bytes)
-                ?(content_bytes-file_count_bytes)
+            ,((block_bytes>(header.content_bytes-file_count_bytes)
+                ?(header.content_bytes-file_count_bytes)
                 :block_bytes))
             ,this->package_path_string_
         );
-        package_count_bytes+=read_bytes;
+        this->package_count_bytes_+=read_bytes;
         //解码content的文件密钥xor混淆
         for(::std::uint64_t index=0;index<read_bytes;++index){
             block[index]=static_cast<char>(
-                static_cast<::std::uint8_t>(block[index])^key
+                static_cast<::std::uint8_t>(block[index])^header.key
             );
         }
         //将分块读取的content写入文件
@@ -207,95 +205,74 @@ void Unpacker::unpack_content(
         );
         file_count_bytes+=read_bytes;
     }
-    if(file_count_bytes!=content_bytes){
+    if(file_count_bytes!=header.content_bytes){
         FGWSZ_THROW_WHAT("file write incomplete: "+file_path_string);
     }
 }
 void Unpacker::unpack_package(::std::filesystem::path const& output_dir_path){
-    //========================================================================
     //输入参数检查阶段
-    //========================================================================
     ::fgwsz::try_create_directories(output_dir_path);
     ::fgwsz::path_assert_is_directory(output_dir_path);
-
+    //重置包文件流到头部和重置包读取字节计数器为0
+    this->reset_package();
     //用于读取文件内容信息的内存块
-    constexpr ::std::uint64_t block_bytes=1024*1024;//1MB
     //MSVC中栈全部内存默认1MB,直接分配在栈上会导致栈溢出,改为分配在堆上
-    auto block_body=::std::make_unique<char[]>(block_bytes);
-    char* block=block_body.get();
-    //用于记录已读取包内容字节数的计数器
-    ::std::uint64_t package_count_bytes=0;
+    constexpr ::std::uint64_t block_bytes=1024*1024;//1MB
+    auto block=::std::make_unique<char[]>(block_bytes);
     //文件头信息变量
     ::fgwsz::Unpacker::Header header;
-    while(package_count_bytes<(this->package_bytes_)){
-        //====================================================================
+    while(this->package_count_bytes_<this->package_bytes_){
         //文件头信息处理阶段
-        //====================================================================
-        this->unpack_header(package_count_bytes,header);
-        //====================================================================
+        this->unpack_header(header);
         //文件内容信息处理阶段
-        //====================================================================
         this->unpack_content(
-            package_count_bytes
-            ,key
-            ,block
+            output_dir_path
+            ,header
+            ,block.get()
             ,block_bytes
-            ,output_dir_path
-            ,relative_path_string
-            ,content_bytes
         );
     }
-    if(package_count_bytes!=this->package_bytes_){
+    if(this->package_count_bytes_!=this->package_bytes_){
         FGWSZ_THROW_WHAT(
             "package read incomplete: "+this->package_path_string_
         );
     }
 }
-//显示包内的文件信息
 void Unpacker::list_package(void){
-    //用于记录已读取包内容字节数的计数器
-    ::std::uint64_t package_count_bytes=0;
+    //重置包文件流到头部和重置包读取字节计数器为0
+    this->reset_package();
+    //文件头信息变量
+    ::fgwsz::Unpacker::Header header;
     //文件id
     ::std::uint64_t file_id=0;
-    //文件头信息变量
-    ::std::uint8_t key=0;
-    ::std::uint64_t relative_path_bytes=0;
-    ::std::string relative_path_string={};
-    ::std::uint64_t content_bytes=0;
-    while(package_count_bytes<(this->package_bytes_)){
-        ::fgwsz::cout<<"file["<<file_id<<"]:\n";
-        //====================================================================
+    while(this->package_count_bytes_<this->package_bytes_){
         //文件头信息处理阶段
-        //====================================================================
-        //读取key
-        key=this->unpack_key(package_count_bytes);
-        ::fgwsz::cout<<"\tkey: "<<static_cast<unsigned>(key)<<'\n';
-        //读取relative path bytes
-        relative_path_bytes=
-            this->unpack_relative_path_bytes(package_count_bytes,key);
-        ::fgwsz::cout<<"\trelative path bytes: "<<relative_path_bytes<<'\n';
-        //读取relative path
-        relative_path_string=this->unpack_relative_path_string(
-            package_count_bytes,key,relative_path_bytes
+        this->unpack_header(header);
+        ::fgwsz::cout<<::std::format(
+            "file[{}]: {{\n"
+            "\tkey: {}\n"
+            "\trelative path bytes: {}\n"
+            "\trelative path string: {}\n"
+            "\tcontent bytes: {}\n"
+            "}}\n"
+            ,file_id
+            ,static_cast<unsigned>(header.key)
+            ,header.relative_path_bytes
+            ,header.relative_path_string
+            ,header.content_bytes
         );
-        ::fgwsz::cout<<"\trelative path: "<<relative_path_string<<'\n';
-        //读取content bytes
-        content_bytes=this->unpack_content_bytes(package_count_bytes,key);
-        ::fgwsz::cout<<"\tcontent bytes: "<<content_bytes<<'\n';
-        //====================================================================
         //文件内容信息跳过阶段
-        //====================================================================
-        this->package_.seekg(content_bytes,::std::ios::cur);
+        this->package_.seekg(header.content_bytes,::std::ios::cur);
         if(!this->package_.good()){
             FGWSZ_THROW_WHAT(
-                "failed to skip content bytes: "+relative_path_string
+                "failed to skip content bytes: "+header.relative_path_string
             );
         }
-        package_count_bytes+=content_bytes;
+        this->package_count_bytes_+=header.content_bytes;
         //更新文件id
         ++file_id;
     }
-    if(package_count_bytes!=this->package_bytes_){
+    if(this->package_count_bytes_!=this->package_bytes_){
         FGWSZ_THROW_WHAT(
             "package read incomplete: "+this->package_path_string_
         );
--- source/fgwsz_unpacker.h
+++ source/fgwsz_unpacker.h
@@ -1,7 +1,7 @@
 #ifndef FGWSZ_UNPACKER_H
 #define FGWSZ_UNPACKER_H
 
-#include<cstdint>   //::std::uint64_t
+#include<cstdint>   //::std::uint8_t ::std::uint64_t
 
 #include<string>    //::std::string
 #include<fstream>   //::std::ifstream
@@ -27,33 +27,32 @@ private:
         ::std::string relative_path_string;
         ::std::uint64_t content_bytes;
     };
-    ::std::uint8_t unpack_key(::std::uint64_t& package_count_bytes);
-    ::std::uint64_t unpack_relative_path_bytes(
-        ::std::uint64_t& package_count_bytes
-        ,::std::uint8_t key
+    void reset_package(void);
+    void unpack_key(::std::uint8_t& key);
+    void unpack_relative_path_bytes(
+        ::std::uint8_t key
+        ,::std::uint64_t& relative_path_bytes
     );
-    ::std::string unpack_relative_path_string(
-        ::std::uint64_t& package_count_bytes
-        ,::std::uint8_t key
+    void unpack_relative_path_string(
+        ::std::uint8_t key
         ,::std::uint64_t relative_path_bytes
+        ,::std::string& relative_path_string
     );
-    ::std::uint64_t unpack_content_bytes(
-        ::std::uint64_t& package_count_bytes
-        ,::std::uint8_t key
+    void unpack_content_bytes(
+        ::std::uint8_t key
+        ,::std::uint64_t& content_bytes
     );
-    void unpack_header(
-        ::std::uint64_t& package_count_bytes
-        ,Header& header
-    );
+    void unpack_header(Header& header);
     void unpack_content(
-        ::std::uint64_t& package_count_bytes
-        ,Header const& header
+        ::std::filesystem::path const& output_dir_path
+        ,Unpacker::Header const& header
         ,char* block
         ,::std::uint64_t block_bytes
     );
     ::std::ifstream package_;
     ::std::string package_path_string_;
     ::std::uint64_t package_bytes_;
+    ::std::uint64_t package_count_bytes_;
 };
 
 }//namespace fgwsz
